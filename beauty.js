552f084f752c982fc01372e4b6ee9eda5d7f364e
a-cole55 <arniqua_c@yahoo.com>
2022-03-17 15:42:25 -0500

    initial commit

---
+++ beauty.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+//make it so that when you toggle over images they get bigger
+// document.querySelector

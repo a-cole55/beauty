a1a1a239d805c6c93a0f4bec315ce52ac4d5890d
a-cole55 <arniqua_c@yahoo.com>
2022-03-19 16:52:35 -0500

    changes to responsive layout-mobile updated lookbook and services

--- beauty.js
+++ beauty.js
@@ -9,3 +9,14 @@ function scrollFunction() {
 }
 //make it so that when you toggle over images they get bigger
 // document.querySelector
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
